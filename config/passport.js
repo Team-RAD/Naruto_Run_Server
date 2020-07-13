560bca851145ab39b06616a4004335c2779cb2f3
David Johnson <david_jono@hotmail.com>
2020-07-13 20:24:40 +1000

    added passport, auth and user routes

---
+++ config/passport.js
@@ -0,0 +1,8 @@
+const passport = require("passport");
+const User = require("../models/user");
+//set up passport-local strategy with correct options
+passport.use(User.createStrategy());
+//middleware generates a function that serializes users into sessions
+passport.serializeUser(User.serializeUser());
+//middleware generates a function that deserializes users into sessions
+passport.deserializeUser(User.deserializeUser());
